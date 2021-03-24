620820122e30d3fd8503848958a268560a45da69
Maximilien Feliu <maximilien.feliu@outlook.com>
2021-03-24 01:22:52 +0100

    rajout de l'apparition du texte dans 'Slides.js'

--- js/Slides.js
+++ js/Slides.js
@@ -5,23 +5,38 @@ class Slides {
     }
 
     defileImg () {
-
-        let divCarousel = document.getElementsByClassName('head_slides')[0];
+        let divSlides = document.getElementsByClassName('head_slides')[0];
         let counter = 1;
-        divCarousel.style.background = 'url(../img/slides/img_slide0.webp) no-repeat fixed center';
-        divCarousel.style.backgroundSize = 'cover';
+        divSlides.style.background = 'url(../img/slides/img_slide0.webp) no-repeat fixed center';
+        divSlides.style.backgroundSize = 'cover';
+
+        let titleAppear = document.getElementsByClassName('title_appear0')[0];
+        titleAppear.style.display = 'block';
+
         setInterval(change, 5000); 
 
-        function change() {        
-                divCarousel.style.background = 'url(../img/slides/img_slide'+ counter + '.webp) no-repeat fixed center';
-                divCarousel.style.backgroundSize = 'cover';
+        function change() {     
+
+                divSlides.style.background = 'url(../img/slides/img_slide'+ counter + '.webp) no-repeat fixed center';
+                divSlides.style.backgroundSize = 'cover';
+
+                let titleAppear = document.getElementsByClassName('title_appear'+ counter)[0];
+                titleAppear.style.display = 'block';  
+
+                if(counter > 0) { 
+                    let titleRemove = document.getElementsByClassName('title_appear'+ (counter - 1))[0];
+                    titleRemove.style.display = 'none';  
+                }else {
+                    let titleRemove = document.getElementsByClassName('title_appear3')[0];
+                    titleRemove.style.display = 'none';
+                }
+
                 counter++; 
 
                 if(counter == 4) {
-                    divCarousel.style.background = 'url(../img/slides/img_slide3.webp) no-repeat fixed bottom';
-                    divCarousel.style.backgroundSize = 'cover';
+                    divSlides.style.background = 'url(../img/slides/img_slide3.webp) no-repeat fixed bottom';
+                    divSlides.style.backgroundSize = 'cover';
                 }
-
                 if (counter > 3) { 
                     counter = 0;
                 }            
